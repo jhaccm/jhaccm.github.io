d87a8e7ffed03928c0854f08d922b6a64f1a80c4
Jeffrey Wang <jeffrey.bond.wang@gmail.com>
2025-04-30 12:34:33 -0400

    0.2.0 Release - Search now working fully. Emergency Drawer has all topics

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2365747693"
+    serviceWorkerVersion: "1697533715"
   }
 });
