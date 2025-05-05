ef8677bbe8b42464d97451939509f48540618a2f
Jeffrey Wang <jeffrey.bond.wang@gmail.com>
2025-05-04 21:44:02 -0400

    0.3.0 Release - Directory of door codes and phone numbers. Releasing to Hopkins ACCM Residents!

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1232215239"
+    serviceWorkerVersion: "2092091899"
   }
 });
