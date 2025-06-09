d0e81ce110d2906585161060e49cff210fa719d5
Jeffrey Wang <jeffrey.bond.wang@gmail.com>
2025-06-08 22:42:51 -0400

    1.0 Release: Drug Calculator implemented

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -63,16 +63,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "7345f79d29fe535f6598cadb29c6bae8
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8d9c1f2c33f26eb6416f6e304fb13cdb",
+"flutter_bootstrap.js": "d999cbed1c23ee1e7403858513af2408",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "853f953db155694b6b103071e38d3cab",
 "/": "853f953db155694b6b103071e38d3cab",
-"main.dart.js": "1dd846614449ed47e28b6e1ffd9ae295",
+"main.dart.js": "78414eb197b5a38bca456a78b784a164",
 "manifest.json": "cb86bfb45a0384f2869063c2caebb0fa",
-"version.json": "d7371399a576a7fb1058b31563eaa118"};
+"version.json": "fa4bfeb40313d72715bf13d4314a9c55"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
